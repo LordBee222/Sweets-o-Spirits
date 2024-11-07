78788af4d6c88e611d5e06d1b463bf3fd0206d7b
GrassBlock210 <macklyn.smith.nz@gmail.com>
2024-11-07 16:21:03 +1300

    Initial commit

---
+++ src/main/java/net/beelabs/sos/SweetsOSpiritsClient.java
@@ -0,0 +1,11 @@
+package net.beelabs.sos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SweetsOSpiritsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
